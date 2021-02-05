95d562797a4bf072590d2ce5f80c12cc6f9868a4
Tore <ytterdorr@gmail.com>
2021-02-05 02:39:40 +0100

    starting on the bluetooth button clicker

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, Pressable } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [multiClickTimer, setMultiClickTimer] = useState();
+  const [multiClickCount, setMultiClickCount] = useState(1);
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [testText, setTestText] = useState();
+
+  const onTimeUp = () => {
+    setTestText("Time up: multiclicks " + multiClickCount);
+    setCount(count => count + 1);
+    setMultiClickCount(1);
+    setTimerRunning(false);
+  }
+
+  const onPress = (event) => {
+    console.log("Button press!");
+    clearTimeout(multiClickTimer);
+    console.log("mcc before update: ", multiClickCount)
+    setMultiClickCount(multiClickCount => multiClickCount + 1);
+    setMultiClickTimer(setTimeout(onTimeUp, 500));
+    console.log(multiClickCount);
+  }
+  const onLongPress = (event) => {
+    setCount(prevCount => prevCount + 5);
+  }
+
+  const focusButton = React.createRef(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.countContainer}>
+        <Text>Count: {count}</Text>
+        <Text>{testText}</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.firstButton}
+        onPress={onPress}
+        onLongPress={onLongPress}
+      >
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+        ref={focusButton}
+      >
+        <Text>Second Press</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingHorizontal: 10
   },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+  countContainer: {
+    alignItems: "center",
+    padding: 10
+  },
+  firstButton: {
+    position: "absolute",
+    top: -40,
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  }
 });
+
+export default App;
